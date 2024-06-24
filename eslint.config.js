bb51f2fb55b5632057937b97b545a001d23d8971
mgamas <manuelmaestro10@gmail.com>
2024-06-24 00:30:48 -0600

    eslint

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      'no-unused-vars': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+    },
+  },
+];
